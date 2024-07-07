0a4b667f37d4ea88030f860e4cd667f388457009
MRoyhanF <froyhan0@gmail.com>
2024-07-08 03:20:36 +0700

    setup route and page for user page

---
+++ views/pages/pelanggan/produk/detail.php
@@ -0,0 +1,245 @@
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs">
+        <div class="page-header d-flex align-items-center" style="background-image: url('');">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+            <div class="col-lg-6 text-center">
+                <h2>Produk</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, impedit!</p>
+            </div>
+            </div>
+        </div>
+        </div>
+        <nav>
+            <div class="container">
+                <ol>
+                <li><a href="/">Home</a></li>
+                <li>Produk</li>
+                </ol>
+            </div>
+        </nav>
+    </div>
+
+    <!-- ======= Blog Details Section ======= -->
+    <section id="blog" class="blog">
+        <div class="container" data-aos="fade-up">
+            <div class="row g-5">
+                <div class="col-lg-8">
+                    <!-- Artikel -->
+                    <article class="blog-details">
+                        <div class="post-img">
+                            <img src="../assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
+                        </div>
+                        <h2 class="title">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</h2>
+                        <div class="meta-top">
+                            <ul>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="#">John Doe</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time datetime="2020-01-01">Jan 1, 2022</time></a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="#">12 Comments</a></li>
+                            </ul>
+                        </div><!-- End meta top -->
+
+                        <div class="content">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vitae.</p>
+                            <blockquote>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, eligendi?</p>
+                            </blockquote>
+                            <!-- <img src="../assets/img/blog/blog-inside-post.jpg" class="img-fluid" alt=""> -->
+                            <h3>Et quae iure vel ut odit alias.</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, nihil.</p>
+
+                        </div><!-- End post content -->
+
+                        <div class="meta-bottom">
+                            <i class="bi bi-folder"></i>
+                            <ul class="cats">
+                                <li><a href="#">Business</a></li>
+                            </ul>
+
+                            <i class="bi bi-tags"></i>
+                            <ul class="tags">
+                                <li><a href="#">Creative</a></li>
+                                <li><a href="#">Tips</a></li>
+                                <li><a href="#">Marketing</a></li>
+                            </ul>
+                        </div><!-- End meta bottom -->
+
+                    </article>
+
+                    <!-- owner -->
+                    <div class="post-author d-flex align-items-center">
+                        <img src="../assets/img/blog/blog-author.jpg" class="rounded-circle flex-shrink-0" alt="">
+                        <div>
+                        <h4>Jane Smith</h4>
+                        <div class="social-links">
+                            <a href="https://twitters.com/#"><i class="bi bi-twitter"></i></a>
+                            <a href="https://facebook.com/#"><i class="bi bi-facebook"></i></a>
+                            <a href="https://instagram.com/#"><i class="biu bi-instagram"></i></a>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, quis.</p>
+                        </div>
+                    </div>
+
+                    <!-- coments -->
+                    <div class="comments">
+                        <h4 class="comments-count">8 Comments</h4>
+                        <!-- coment -->
+                        <div id="comment-1" class="comment">
+                            <div class="d-flex">
+                                <div class="comment-img"><img src="../assets/img/blog/comments-1.jpg" alt=""></div>
+                                <div>
+                                    <h5><a href="">Georgia Reader</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5>
+                                    <time datetime="2020-01-01">01 Jan,2022</time>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, amet?</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="comment-2" class="comment">
+                            <div class="d-flex">
+                                <div class="comment-img"><img src="../assets/img/blog/comments-2.jpg" alt=""></div>
+                                <div>
+                                    <h5><a href="">Aron Alvarado</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5>
+                                    <time datetime="2020-01-01">01 Jan,2022</time>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, saepe.</p>
+                                </div>
+                            </div>
+
+                            <div id="comment-reply-1" class="comment comment-reply">
+                                <div class="d-flex">
+                                    <div class="comment-img"><img src="../assets/img/blog/comments-3.jpg" alt=""></div>
+                                        <div>
+                                            <h5><a href="">Lynda Small</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5>
+                                            <time datetime="2020-01-01">01 Jan,2022</time>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, assumenda!</p>
+                                        </div>
+                                    </div>
+
+                                    <div id="comment-reply-2" class="comment comment-reply">
+                                        <div class="d-flex">
+                                            <div class="comment-img"><img src="../assets/img/blog/comments-4.jpg" alt=""></div>
+                                                <div>
+                                                    <h5><a href="">Sianna Ramsay</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5>
+                                                    <time datetime="2020-01-01">01 Jan,2022</time>
+                                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, reprehenderit?</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div id="comment-3" class="comment">
+                                    <div class="d-flex">
+                                        <div class="comment-img"><img src="../assets/img/blog/comments-5.jpg" alt=""></div>
+                                        <div>
+                                            <h5><a href="">Nolan Davidson</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5>
+                                            <time datetime="2020-01-01">01 Jan,2022</time>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quos.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="comment-4" class="comment">
+                                    <div class="d-flex">
+                                        <div class="comment-img"><img src="../assets/img/blog/comments-6.jpg" alt=""></div>
+                                        <div>
+                                            <h5><a href="">Kay Duggan</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5>
+                                            <time datetime="2020-01-01">01 Jan,2022</time>
+                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, quia.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- reply form -->
+                                <div class="reply-form">
+                                    <h4>Leave a Reply</h4>
+                                    <p>Your email address will not be published. Required fields are marked * </p>
+                                    <form action="">
+                                        <div class="row">
+                                            <div class="col-md-6 form-group">
+                                                <input name="name" type="text" class="form-control" placeholder="Your Name*">
+                                            </div>
+                                            <div class="col-md-6 form-group">
+                                                <input name="email" type="text" class="form-control" placeholder="Your Email*">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col form-group">
+                                                <input name="website" type="text" class="form-control" placeholder="Your Website">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col form-group">
+                                                <textarea name="comment" class="form-control" placeholder="Your Comment*"></textarea>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Post Comment</button>
+                                    </form>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!-- sidebar -->
+                        <div class="col-lg-4">
+                            <div class="sidebar">
+                                <!-- serach -->
+                                <div class="sidebar-item search-form">
+                                    <h3 class="sidebar-title">Search</h3>
+                                    <form action="" class="mt-3">
+                                        <input type="text">
+                                        <button type="submit"><i class="bi bi-search"></i></button>
+                                    </form>
+                                </div>
+
+                                <!-- categori -->
+                                <div class="sidebar-item categories">
+                                    <h3 class="sidebar-title">Categories</h3>
+                                    <ul class="mt-3">
+                                        <li><a href="#">General <span>(25)</span></a></li>
+                                        <li><a href="#">Lifestyle <span>(12)</span></a></li>
+                                    </ul>
+                                </div>
+
+                                <!-- recent post list -->
+                                <div class="sidebar-item recent-posts">
+                                    <h3 class="sidebar-title">Recent Posts</h3>
+                                    <div class="mt-3">
+                                        <!-- recent post item -->
+                                        <div class="post-item mt-3">
+                                            <img src="../assets/img/blog/blog-recent-1.jpg" alt="">
+                                            <div>
+                                                <h4><a href="blog-details.html">Nihil blanditiis at in nihil autem</a></h4>
+                                                <time datetime="2020-01-01">Jan 1, 2020</time>
+                                            </div>
+                                        </div>
+                                        <!-- recent post item -->
+                                        <div class="post-item">
+                                            <img src="../assets/img/blog/blog-recent-2.jpg" alt="">
+                                            <div>
+                                                <h4><a href="blog-details.html">Quidem autem et impedit</a></h4>
+                                                <time datetime="2020-01-01">Jan 1, 2020</time>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- tags list -->
+                                <div class="sidebar-item tags">
+                                    <h3 class="sidebar-title">Tags</h3>
+                                    <ul class="mt-3">
+                                        <li><a href="#">App</a></li>
+                                        <li><a href="#">IT</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
