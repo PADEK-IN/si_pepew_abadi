74e1a8c3107a099a6cdb7237f21047e02cc90092
fajrCode <fajri42vandi@gmail.com>
2024-07-07 13:44:22 +0700

    init: index root apps

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+// Load the config app
+require_once '../config/app.php';
+require_once '../config/db.php';
+
+// Load the routes
+require_once '../routes/index.php';
+
+// Handle the request
+handleRequest($_SERVER['REQUEST_URI']);
+
+?>
