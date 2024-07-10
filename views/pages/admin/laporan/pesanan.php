7a0c9247ea311e73c779b074bcc9268139e1a50d
MRoyhanF <froyhan0@gmail.com>
2024-07-10 15:42:15 +0700

    slicing pemenasan page at admin event still confuse

--- views/pages/admin/laporan/pesanan.php
+++ views/pages/admin/laporan/pesanan.php
@@ -1,4 +1,80 @@
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet eligendi tempora? Veniam excepturi illum fugit error modi aliquam blanditiis!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet eligendi tempora? Veniam excepturi illum fugit error modi aliquam blanditiis!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet eligendi tempora? Veniam excepturi illum fugit error modi aliquam blanditiis!</p>
-<p>laporan pesanan</p>
+<div class="container">
+    <div class="page-inner">
+        <div class="page-header">
+            <ul class="breadcrumbs" style="margin:0;">
+                <li class="nav-home">
+                    <a href="/admin/dashboard">
+                    <i class="icon-home"></i>
+                    </a>
+                </li>
+                <li class="separator">
+                    <i class="icon-arrow-right"></i>
+                </li>
+                <li class="nav-item">
+                    <b>Laporan Pesanan</b>
+                </li>
+            </ul>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <h4 class="card-title">Daftar List Pesanan</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="multi-filter-select" class="display table table-striped table-hover text-center">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Code</th>
+                                        <th>Nama Penerima</th>
+                                        <th>Nama Barang</th>
+                                        <th>Jumlah</th>
+                                        <th>Harga</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Penerima</th>
+                                        <th>Nama Barang</th>
+                                        <th>Jumlah</th>
+                                        <th>Harga</th>
+                                        <th>Total</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>
+                                            <a href="/admin/pesanan/detail">GFDYT237</a>
+                                        </td>
+                                        <td>Santorio widodo</td>
+                                        <td>Selimut</td>
+                                        <td>3</td>
+                                        <td>Rp. 50.000</td>
+                                        <td>Rp. 150.000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>
+                                            <a href="/admin/pesanan/detail">73D82H2</a>
+                                        </td>
+                                        <td>Santorio widodo</td>
+                                        <td>Selimut</td>
+                                        <td>3</td>
+                                        <td>Rp. 50.000</td>
+                                        <td>Rp. 150.000</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- views/pages/admin/laporan/produk.php
+++ views/pages/admin/laporan/produk.php
@@ -1,4 +1,76 @@
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet eligendi tempora? Veniam excepturi illum fugit error modi aliquam blanditiis!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet eligendi tempora? Veniam excepturi illum fugit error modi aliquam blanditiis!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet eligendi tempora? Veniam excepturi illum fugit error modi aliquam blanditiis!</p>
-<p>laporan produk</p>
+<div class="container">
+    <div class="page-inner">
+        <div class="page-header">
+            <ul class="breadcrumbs" style="margin:0;">
+                <li class="nav-home">
+                    <a href="/admin/dashboard">
+                    <i class="icon-home"></i>
+                    </a>
+                </li>
+                <li class="separator">
+                    <i class="icon-arrow-right"></i>
+                </li>
+                <li class="nav-item">
+                    <b>Laporan Barang</b>
+                </li>
+            </ul>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <h4 class="card-title">Daftar List Barang</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                        <table id="multi-filter-select" class="display table table-striped table-hover text-center">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Kode</th>
+                                        <th>Nama Barang</th>
+                                        <th>Harga</th>
+                                        <th>Image</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Kode</th>
+                                        <th>Nama Barang</th>
+                                        <th>Harga</th>
+                                        <th>Image</th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>
+                                            5FGH432H
+                                        </td>
+                                        <td>Sarung Bantal</td>
+                                        <td>Rp. 20.000</td>
+                                        <td>
+                                            <img src="../../assets/auth/img/register.jpg" alt="produk1" width="100px">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>
+                                            5FGH432H
+                                        </td>
+                                        <td>Sarung Bantal</td>
+                                        <td>Rp. 20.000</td>
+                                        <td>
+                                            <img src="../../assets/auth/img/register.jpg" alt="produk1" width="100px">
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
