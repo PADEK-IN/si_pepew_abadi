5be50a326f7980dbf6babe7d24d8a0cfee41884b
MRoyhanF <froyhan0@gmail.com>
2024-07-13 00:19:47 +0700

    update card

--- views/components/pelanggan/card-produk.php
+++ views/components/pelanggan/card-produk.php
@@ -5,20 +5,12 @@
         <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
         </div>
 
-        <p class="post-category">Politics</p>
-
         <h2 class="title">
-        <a href="/produk/1">Dolorum optio tempore voluptas dignissimos</a>
+            <a href="/produk/1">CPN 15-0-14/KNO3 Merah</a>
         </h2>
-
-        <div class="d-flex align-items-center">
-        <img src="assets/img/blog/blog-author.jpg" alt="" class="img-fluid post-author-img flex-shrink-0">
-        <div class="post-meta">
-            <p class="post-author-list">Maria Doe</p>
-            <p class="post-date">
-            <time datetime="2022-01-01">Jan 1, 2022</time>
-            </p>
-        </div>
+        <div class="d-flex justify-content-between">
+            <p class="post-author-list">Pupuk</p>
+            <small><u>Kilogram (KG)</u></small>
         </div>
 
     </article>
